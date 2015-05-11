1e880324ec9ce2d3f03a2f009b427610743a5ad7
Louka Desroziers <ldesroziers@me.com>
2015-05-11 16:52:03 +0200

    Fixes popover detection

--- Keyboard/UIViewController+PopoverDetection.swift
+++ Keyboard/UIViewController+PopoverDetection.swift
@@ -8,8 +8,15 @@ This extension of UIViewController fixes that issue by implementing a simple 'is
 public extension UIViewController {
     public func isBeingDisplayedAsPopover() -> Bool {
         if let popoverPresentationController = self.popoverPresentationController {
-            return popoverPresentationController.traitCollection.horizontalSizeClass == .Compact
+            return popoverPresentationController.adaptivePresentationStyle() == .Popover
+                || popoverPresentationController.adaptivePresentationStyle() == .None
         }
+        /// In some cases, this fails… we cannot get the popoverPresentationController from a UIViewController… so we must suggest that if the presentingViewController is compact, so is the presented…
+        if let presentationController = self.presentationController {
+            return presentationController.presentationStyle == .Popover
+                || self.presentingViewController?.traitCollection.horizontalSizeClass == .Regular
+        }
+        
         return false
     }
 }
